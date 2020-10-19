7e45542bba93eb13e5bab4b81afa36606aabffa0
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2020-10-18 20:01:40 -0400

    Trying to figure out what database drivers are available by default

--- databasehelper.cpp
+++ databasehelper.cpp
@@ -5,11 +5,14 @@
 DatabaseHelper::DatabaseHelper() {
     //hm, check what's available on the laptop. On Windows, I have QSQLITE QODBC QODBC3 QPSQL QPSQL7
     //the article recommended QMYSQL
-    cadetDB = QSqlDatabase::addDatabase("QMYSQL", "cadets");
+    cadetDB = QSqlDatabase::addDatabase("QSQLITE", "cadets");
     cadetDB.setHostName("localhost");
     cadetDB.setDatabaseName("cadets");
     cadetDB.setUserName("office");
     cadetDB.setPassword("MAR-DC-045");
     bool ok = cadetDB.open();
+    if(!ok){
+        qDebug() << "database error: " << cadetDB.lastError().text() << "\n";
+    }
     qDebug() << "Database open success: " << ok << "\n";
 }
--- databasehelper.h
+++ databasehelper.h
@@ -1,7 +1,7 @@
 #ifndef DATABASEHELPER_H
 #define DATABASEHELPER_H
 
-#include <QtSql/QSqlDatabase>
+#include <QtSql>
 
 class DatabaseHelper
 {
