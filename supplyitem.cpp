f4e70e33b120a60faac048da0a42a11f816752c2
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2020-10-28 12:02:08 -0400

    Preparing for new class, don't have access to Qt Creator, so these could appear incorrect

---
+++ supplyitem.cpp
@@ -0,0 +1,5 @@
+#include "supplyitem.h"
+
+SupplyItem::SupplyItem(){
+    
+}
---
+++ supplyitem.h
@@ -0,0 +1,12 @@
+#ifndef SUPPLYITEM_H
+#define SUPPLYITEM_H
+
+
+class SupplyItem {
+public:
+
+    SupplyItem();
+
+};
+
+#endif // SUPPLYITEM_H
