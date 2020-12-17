8e67fc5e228435e30e3d15be820714954961a3b9
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2020-12-17 12:35:26 -0500

    Added JavaDoc-style headers to variables and functions

--- datamanager.h
+++ datamanager.h
@@ -17,19 +17,45 @@
 #include <QJsonDocument>
 #include <QFile>
 
+/**
+ * @brief An interface between the program and the files that store the data for this application.
+ */
 class DataManager {
 public:
 
     DataManager();
 
+	/**
+	 * @brief All the cadets that have been entered into the program, indexed by their UUID.
+	 */
 	static QMap<QString, Cadet> cadets;
+	/**
+	 * @brief All the supply items that have been entered into the program, indexed by their UUID.
+	 */
 	static QMap<QString, SupplyItem> items;
+	/**
+	 * @brief All the inspection logs that have been entered into the program, indexed by their UUID.
+	 */
 	static QMap<QString, InspectionCard> insCards;
 
+	/**
+	 * @brief Reads all data from a QJsonObject and loads it into memory.
+	 * @param The JSON file, converted into a QJsonObject.
+	 */
     static void read(const QJsonObject& json);
+	/**
+	 * @brief Converts data from memory to a QJsonObject to be written to a file.
+	 * @param The QJsonObject, to be converted into a file later.
+	 */
     static void write(QJsonObject& json);
 
+	/**
+	 * @brief Converts a JSON file to a QJsonObject and then stores it.
+	 */
     static void readFromFile();
+	/**
+	 * @brief Writes stored data out to a JSON file.
+	 */
     static void writeToFile();
 
 };
