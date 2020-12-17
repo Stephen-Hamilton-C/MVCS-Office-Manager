710b657ff841d1d58bd82713b44fff0690a3c5a6
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2020-12-17 12:02:49 -0500

    Added JavaDoc-style headers to variables and functions

--- cadeteditor.h
+++ cadeteditor.h
@@ -15,25 +15,45 @@ namespace Ui {
 class CadetEditor;
 }
 
+/**
+ * @brief Contains UI logic for creating and editing CAP Cadets or Senior Members
+ */
 class CadetEditor : public QMainWindow
 {
     Q_OBJECT
 
 public:
+	/**
+	 * @brief Creates a new CadetEditor window
+	 * @param The UUID of a Cadet to edit. Leave blank if creating a new Cadet.
+	 */
 	explicit CadetEditor(QString id = "", QWidget *parent = nullptr);
     ~CadetEditor();
 
 private slots:
 
+	/**
+	 * @brief Triggered when the member grade is changed
+	 * @param The new index of the gradeBox
+	 */
     void on_gradeBox_currentIndexChanged(int index);
 
+	/**
+	 * @brief Triggered when Return or OK is pressed
+	 */
 	void on_buttonBox_accepted();
 
+	/**
+	 * @brief Triggered when ESC or Cancel is pressed
+	 */
 	void on_buttonBox_rejected();
 
 private:
     Ui::CadetEditor *ui;
 
+	/**
+	 * @brief The UUID of a Cadet stored in `DataManager` to edit, or blank if creating a new Cadet.
+	 */
 	QString id = "";
 };
 
