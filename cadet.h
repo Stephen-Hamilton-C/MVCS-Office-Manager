35c56af35c88567ff3254a4657aec621a271388c
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2020-12-16 22:32:11 -0500

    Added JavaDoc-style headers to variables and functions

--- cadet.h
+++ cadet.h
@@ -76,6 +76,13 @@ public:
         UNASSIGNED
     };
     
+	/**
+	 * @brief How a name should be formatted.
+	 * LASTFIRST: Last name, First name (Hamilton, Stephen)
+	 * FIRSTLAST: First name Last name (Stephen Hamilton)
+	 * GRADELASTFIRST: Grade/Rank Last name, First name (C/2Lt Hamilton, Stephen)
+	 * GRADEFIRSTLAST: Grade/Rank First name Last name (C/2Lt Stephen Hamilton)
+	 */
     enum NAMEFORMAT {
         LASTFIRST,
         FIRSTLAST,
@@ -83,8 +90,10 @@ public:
 		GRADEFIRSTLAST
     };
 
+	///The identifier used in the /class DataManager that indexes this member
 	QString uuid;
     int capid;
+	///Cadet or Senior Member
     GRADE grade;
     RANK rank;
 	QString firstName;
@@ -92,27 +101,94 @@ public:
     FLIGHT flight;
     QString notes;
 
+	///Complete setup constructor
 	Cadet(QString uuid, int capid, GRADE grade, RANK rank, QString firstName, QString lastName, FLIGHT flight, QString notes);
+	///Empty constructor
     Cadet();
 
+	/**
+	 * @brief Converts a RANK enum into a human readable QString. See `Cadet::getShortRankStr(RANK rank)` for a shorter QString.
+	 * @param rank A RANK enum to convert to QString.
+	 * @return A QString that was made from the RANK enum supplied.
+	 */
 	static QString getRankStr(RANK rank);
+	/**
+	 * @brief Converts the member's rank into a human readable QString. See `Cadet::getShortRankStr()` for a shorter QString.
+	 * @return A QString that was made from the member's rank.
+	 */
 	QString getRankStr() const;
+	/**
+	 * @brief Converts a RANK enum into an abbreviated human readable QString. See `Cadet::getRankStr(RANK rank)` for a longer QString.
+	 * @param rank A RANK enum to convert to an abbreviated QString.
+	 * @return An abbreviated QString that was made from the RANK enum supplied.
+	 */
 	static QString getShortRankStr(RANK rank);
+	/**
+	 * @brief Converts the member's rank into an abbreviated human readable QString. See `Cadet::getRankStr()` for a longer QString.
+	 * @return An abbreviated QString that was made from the member's rank.
+	 */
 	QString getShortRankStr() const;
+	/**
+	 * @brief Converts a FLIGHT enum into a human readable QString.
+	 * @param flight A FLIGHT enum to convert to QString.
+	 * @return A QString that was made from the FLIGHT enum supplied.
+	 */
 	static QString getFlightStr(FLIGHT flight);
+	/**
+	 * @brief Converts the member's flight into a human readable QString.
+	 * @return A QString that was made from the member's flight.
+	 */
 	QString getFlightStr() const;
+	/**
+	 * @brief Converts a GRADE enum into a human readable QString. See `Cadet::getShortGradeStr(GRADE grade, bool slash = true)` for a shorter QString.
+	 * @param grade A GRADE enum to convert to QString.
+	 * @return A QString that was made from the GRADE enum supplied.
+	 */
 	static QString getGradeStr(GRADE grade);
+	/**
+	 * @brief Converts the member's grade into a human readable QString. See `Cadet::getShorterGradeStr(bool slash = true)` for a shorter QString.
+	 * @return A QString that was made from the member's grade.
+	 */
 	QString getGradeStr() const;
+	/**
+	 * @brief Converts a GRADE enum into an abbreviated human readable QString. See `Cadet::getGradeStr(GRADE grade)` for a longer QString.
+	 * @param grade A GRADE enum to convert into an abbreviated QString.
+	 * @param slash Whether or not to have the slash after the grade (C/ or SM/)
+	 * @return An abbreviated QString that was made from the GRADE enum supplied.
+	 */
 	static QString getShortGradeStr(GRADE grade, bool slash = true);
+	/**
+	 * @brief Converts the member's grade into an abbreviated human readable QString. See `Cadet::getGradeStr()` for a longer QString.
+	 * @param slash Whether or not to have the slash after the grade (C/ or SM/).
+	 * @return An abbreviated QString that was made from the member's grade.
+	 */
 	QString getShortGradeStr(bool slash = true) const;
+	/**
+	 * @brief Returns a formatted QString using the cadet's name and/or rank.
+	 * @param The way the name should be formatted. See `Cadet::NAMEFORMAT` to see what the formatting options do.
+	 * @return A formatted QString.
+	 */
 	QString getFormattedName(NAMEFORMAT format = NAMEFORMAT::LASTFIRST) const;
-	QString getID() const;
-
-    void read(const QJsonObject& json);
-    void write(QJsonObject& json) const;
-
+	/**
+	 * @brief Returns the leadership phase that the cadet is currently in.
+	 * @return an int that resembles the phase.
+	 */
 	int getPhase() const;
 
+	/**
+	 * @brief Sets all variables to values read from the QJsonObject, if they exist, supplied by the `DataManager`.
+	 * @param A reference to a QJsonObject read from a file.
+	 */
+    void read(const QJsonObject& json);
+	/**
+	 * @brief Writes all variables to a QJsonObject to be written to a file later by the `DataManager`.
+	 * @param A reference to a QJsonObject that is to be written to a file.
+	 */
+    void write(QJsonObject& json) const;
+
+	/**
+	 * @brief Debug purposes. Spits out all variables to the qDebug.
+	 */
     QString toString();
 
 };
