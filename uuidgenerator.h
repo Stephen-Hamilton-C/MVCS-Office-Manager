104c5aa09f3f2358b319d95fdc02ace20ffb32c8
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2021-06-07 20:39:34 -0400

    Added UUIDGenerator

---
+++ uuidgenerator.h
@@ -0,0 +1,26 @@
+#ifndef UUIDGENERATOR_H
+#define UUIDGENERATOR_H
+
+#include <QUuid>
+
+class UUIDGenerator
+{
+public:
+    UUIDGenerator();
+
+    enum IDType {
+        STANDARD,
+        CADET,
+        ITEM,
+        CARD
+    };
+
+    static QString generateUUID(IDType idType = IDType::STANDARD, QString uuid = "");
+
+private:
+
+    static void removeBraces(QString &uuid);
+
+};
+
+#endif // UUIDGENERATOR_H
